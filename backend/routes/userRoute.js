f07c5f0584dfa2b71748cf3f1d34222c8ab7762e
Muskan Koshti <103520140+muskank101@users.noreply.github.com>
2024-03-03 16:02:40 +0530

    upd

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -64,4 +64,20 @@ router.delete("/:id",async(req,res)=>{
 
 });
 
+// update
+
+router.patch("/:id",async(req,res)=>{
+    const {id} = req.params;
+    const{name,email,age}=req.body;
+    try{
+        const updateUser =await User.findByIdAndUpdate(id,req.body,{
+            new:true,
+        });
+        res.status(200).json(updateUser);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:error.message})
+    }
+
+});
 module.exports = router;
